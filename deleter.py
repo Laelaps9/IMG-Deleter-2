9388ac56e3a8066892845f5bf27aa71a48ab0a70
Laelaps9 <Breadbugz@protonmail.com>
2021-08-12 18:56:43 -0500

    Quick deletion functional Make quick deletion delete images properly. When a key that's not defined in the config is pressed, the valid options are printed. Remove parentheses from ifs, fors, and whiles to have a consistent format.

--- deleter.py
+++ deleter.py
@@ -17,7 +17,7 @@ def get_images(path):
     files = os.listdir(path)
     images = []
     for file in files:
-        if (".png" in file or ".jpg" in file):
+        if ".png" in file or ".jpg" in file:
             images.append(os.path.join(path, file))
     return images
 
@@ -37,27 +37,45 @@ def define_keys(skip, delete, prevImg, nextImg, stop):
             }
 
 def get_keys():
-    global config
+    global config, keySkip, keyDelete, keyPrev, keyNext, keyStop
     config = []
     f = open("keys.conf", "r")
     lines = f.readlines()
     for line in lines:
-        if("skip:" in line):
+        if "skip:" in line:
+            keySkip = line
             skip = line.split(":")[1].strip(" \n")
             continue
-        if("delete:" in line):
+        if "delete:" in line:
+            keyDelete = line
             delete = line.split(":")[1].strip(" \n")
             continue
-        if("previous:" in line):
+        if "previous:" in line:
             prevImg = line.split(":")[1].strip(" \n")
             continue
-        if("next:" in line):
+        if "next:" in line:
             nextImg = line.split(":")[1].strip(" \n")
             continue
-        if("stop:" in line):
+        if "stop:" in line:
             stop = line.split(":")[1].strip(" \n")
     
     define_keys(skip, delete, prevImg, nextImg, stop)
+
+def delete_images(images):
+    print("\nImages to be deleted:")
+    for image in images:
+        print(image)
+
+    proceed = input("Are you sure you want to proceed? (y/n): ")
+    if proceed == "y" or proceed == "yes":
+        print("Deleting...")
+        for image in images:
+            os.remove(image)
+
+        print("Complete")
+        return
+
+    print("Cancelled")
 
 def quick_deletion(images):
     print("Quick Deletion")
@@ -68,26 +86,29 @@ def quick_deletion(images):
         cv2.imshow(image, img)
         k = cv2.waitKey(0)
         flag = switch.get(k)
-        while (flag != "Skip" and flag != "Delete"):
+        while flag != "Skip" and flag != "Delete":
             print("Invalid option")
+            print("\t" + keySkip, "\t" + keyDelete)
             k = cv2.waitKey(0)
             flag = switch.get(k)
         
         cv2.destroyAllWindows()
-        if(flag == "Skip"):
-            print("Skip")
-            continue
-        elif(flag == "Delete"):
+        if flag == "Delete":
             print("Delete")
             toDelete.append(image)
+            continue
+
+        print("Skip")
+
+    delete_images(toDelete)
 
 def duplicate_deletion():
     print("Duplicate Deletion")
 
 def get_deletion(option, images):
-    if(option == 1):
+    if option == 1:
         quick_deletion(images)
-    elif(option == 2):
+    elif option == 2:
         duplicate_deletion()
     else:
         print_help("Invalid Option")
@@ -95,11 +116,12 @@ def get_deletion(option, images):
 args = sys.argv[1:]
 
 # If no arguments were given, call get_dir()
-if(len(args) == 0):
+if len(args) == 0:
     path = input("Input the path to the directory (e.g. /home/user/Pictures): ")
 else:
-    if(len(args) > 1):
+    if len(args) > 1:
         print_help("Too many arguments")
+    
     path = args[0]
 
 images = get_images(path)
@@ -112,4 +134,3 @@ Select a deletion mode (number):
 
 get_keys()
 get_deletion(option, images)
-#os.remove(test)
